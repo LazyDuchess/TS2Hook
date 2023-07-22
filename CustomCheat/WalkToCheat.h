5cb22e5528e15d33946496b32e90596ad9aa7a5f
Nahuel Rocchetti <Nahuel3d@gmail.com>
2023-07-22 18:30:11 -0300

    Implement Steal global interaction.

--- CustomCheat/WalkToCheat.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../TS2Hook/Game.h"
-
-class WalkToCheat : public TS::cCheatCommand {
-public:
-	const char* Name();
-	const char* Description(void* commandHelpType);
-	void Execute(nGZCommandParser::cArguments* arguments);
-};
--- CustomInteraction/CustomInteractionScript.cpp
+++ CustomInteraction/CustomInteractionScript.cpp
@@ -2,53 +2,33 @@
 #include "CustomInteractionScript.h"
 #include "../TS2Hook/Input.h"
 
-const char* interactionName = "Reset Object";
-const char* interactionNameSpanish = "Reiniciar Objeto";
-
 void CustomInteractionScript::OnBuildUserDirectedInteractionMenu(std::vector<TS::cTSInteraction*>* interactions, TS::cEdithObjectTestSim* testSim, bool debug) {
-    if (debug)
-        return;
-
-	TS::cTSGlobals* globals = TS::Globals();
-	if (globals == nullptr)
+	if (debug)
 		return;
-	TS::cEdithObjectModule* objectMgr = globals->ObjectManager();
-	if (objectMgr == nullptr)
+	TS::cTSGlobals* pGlobals = TS::Globals();
+	if (pGlobals == nullptr)
 		return;
-	/*
-	std::vector<TS::cTSObject*>* objects = objectMgr->GetObjects();
-	TS::cTSObject* testObject = nullptr;
-
-	for (auto object : *objects)
-	{
-		if (object == nullptr)
-			continue;
-		TS::cTSObjectSelector* selector = object->GetSelector();
-		if (selector == nullptr)
-			continue;
-		if (selector->GetGUID() == 0x53F1F7DC)
-		{
-			testObject = object;
-			break;
-		}
-	}
-
-	if (testObject == nullptr)
+	TS::cEdithObjectModule* pObjectMgr = pGlobals->ObjectManager();
+	if (pObjectMgr == nullptr)
 		return;
-	*/
-	TS::cTSObject* testObject = objectMgr->GetObjectByGUID(0x53F1F7DC);
-	if (testObject == nullptr)
+	// GUID for TS2Hook - Interaction - Steal Object, our global controller that contains our interaction tree in its private scope.
+	TS::cTSObject* pInteractionObject = pObjectMgr->GetObjectByGUID(0xE0F98CA8);
+	if (pInteractionObject == nullptr)
 		return;
-
-	TS::AddCheatInteraction(interactions, testSim->person, testObject, TS::InteractionType::Blocking, 0x32, "Walk To", 0x1006);
-	TS::cTSInteraction* lastInteraction = interactions->back();
-	lastInteraction->SetTargetObject(testSim->object);
-	short stackVars[] = { testSim->object->GetID(), 0, 0, 0 };
-	lastInteraction->SetStackVars(stackVars);
-	//lastInteraction->StackObjectID = testSim->object->GetID();
-		/*
-    const char* localizedName = interactionName;
-    if (TS::LanguageManager()->GetCurrentLanguage() == Language::Spanish)
-        localizedName = interactionNameSpanish;
-    TS::AddCheatInteraction(interactions, testSim->person, testSim->object, TS::InteractionType::Immediate, 0x32, localizedName, 0x2da);*/
+	TS::cTSBehavior* pBehavior = pInteractionObject->GetBehavior();
+	TS::cTSObject* pPersonAsObject = testSim->person->AsEdithObject();
+	short objectId = testSim->object->GetID();
+	// Run the Interaction - Steal TEST check tree on the clicked object.
+	if (!pPersonAsObject->RunTree(pBehavior, objectId, 0x1003, 0, 0, 0, 0))
+		return;
+	// Check tree passed, add Interaction - Steal. We have to pass our interaction controller here rather than the object we clicked as the target.
+	TS::AddCheatInteraction(interactions, testSim->person, pInteractionObject, TS::InteractionType::Blocking, 0x32, "Steal", 0x1002);
+	// Get the interaction we just added.
+	TS::cTSInteraction* newInteraction = interactions->back();
+	// Change the target object so that the interaction runs on the object we clicked, rather than the one with the interaction.
+	newInteraction->SetTargetObject(testSim->object);
+	// Pass the ID of the object we clicked to the interaction as parameter 0. The BHAV tree swaps the stack object to param 0 first thing (Would be nice to automate this)
+	short stackVars[] = { objectId, 0, 0, 0 };
+	newInteraction->SetStackVars(stackVars);
+	// This is hacky, but works to prevent any clashes with private/semiglobal/global scope by technically running the interaction from a separate object.
 }
